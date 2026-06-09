4370af9c6dfb8668cdfbdb1079a6e7c6c40fbbab
Philip Secker <philip.secker@aviatnet.com>
2026-06-09 14:37:43 +1200

    feat: add V_CATALOGUE import to validate_catalogue.py script

--- tools/validate_catalogue.py
+++ tools/validate_catalogue.py
@@ -15,6 +15,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from catalogues.base import Box
 from catalogues.f_catalogue import F_CATALOGUE
 from catalogues.n_catalogue import N_CATALOGUE
+from catalogues.v_catalogue import V_CATALOGUE
 from solvers.decomp import classify, CATALOGUES
 import solvers.decomp as decomp
 
